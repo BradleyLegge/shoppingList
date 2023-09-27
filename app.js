c76f5598f864a041b3b8e5e395f469d6dd28c034
Bradley Legge <bmlegge12@gmail.com>
2023-09-27 10:50:26 -0400

    Add in javascript functionality

--- app.js
+++ app.js
@@ -2,22 +2,31 @@ const itemInput = document.querySelector('#item-input')
 const addBtn = document.querySelector('#add-to-list-btn')
 const toDoList = document.querySelector('#to-do-list')
 const element = document.createElement('p')
-let a = ['abc', 'bcd', 'cde']
 
 
 addBtn.addEventListener('click', getLocalStorage)
 
-function showArray(){
-    console.log(a)
-}
-
-function addToArray(){
-    a.push('def')
-    console.log(a)
-}
-
 function getLocalStorage(){
-    // JSON.parse(localStorage.getItem('list'))
+    let a = (localStorage.getItem('list'))
+    ? JSON.parse(localStorage.getItem('list'))
+    : []
+
+    a.push(itemInput.value)
+
     localStorage.setItem('list', JSON.stringify(a))
+
     console.log(a)
+    itemInput.value = ''
+
+    
 }
+
+function clearArray(){
+    localStorage.clear('list')
+}
+
+
+
+// let a = localStorage.getItem('list')
+// ? JSON.parse(localStorage.getItem('list'))
+// : []
