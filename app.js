1f4c408d71c26de952270476980e768881a395cc
Bradley Legge <bmlegge12@gmail.com>
2023-09-27 21:33:32 -0400

    Added more javascript functionality

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 const itemInput = document.querySelector('#item-input')
 const addBtn = document.querySelector('#add-to-list-btn')
+const clearBtn = document.querySelector('#clear-list')
 const toDoList = document.querySelector('#to-do-list')
-const element = document.createElement('p')
 
 
-addBtn.addEventListener('click', getLocalStorage)
 
-function getLocalStorage(){
-    let a = (localStorage.getItem('list'))
-    ? JSON.parse(localStorage.getItem('list'))
-    : []
+addBtn.addEventListener('click', addToLocalStorage)
+clearBtn.addEventListener('click', clearArray)
+window.addEventListener('DOMContentLoaded', displayList)
 
-    a.push(itemInput.value)
+function addToLocalStorage(){
+    let a = getLocalStorage()
+    let item = itemInput.value
+
+    a.push(item)
 
     localStorage.setItem('list', JSON.stringify(a))
 
-    console.log(a)
     itemInput.value = ''
 
-    
+    createListItem(item)
+}
+
+function getLocalStorage(){
+    return localStorage.getItem('list')
+    ? JSON.parse(localStorage.getItem('list'))
+    : []
+}
+
+function displayList(){
+    let items = getLocalStorage()
+    if(items.length > 0){
+        items.forEach(function(i){
+            createListItem(i)
+        })
+    }
+}
+
+function createListItem(i){
+    const element = document.createElement('li')
+    element.innerHTML = `${i}`
+    toDoList.appendChild(element)
 }
 
 function clearArray(){
